75ae2204fa1c7dab7efbbd5345a3d2a84a16d9cc
Tim Seckinger <seckinger.tim@gmail.com>
2018-12-20 20:01:53 +0100

    build

---
+++ precache-manifest.7a404d1e40560f4db4f9f5c7d0d34021.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26b4b5b8a3098def4ea2",
+    "url": "/bachelor-thesis-slides/static/css/main.08e7b75a.chunk.css"
+  },
+  {
+    "revision": "26b4b5b8a3098def4ea2",
+    "url": "/bachelor-thesis-slides/static/js/main.26b4b5b8.chunk.js"
+  },
+  {
+    "revision": "43973a7fcdfbcfecc426",
+    "url": "/bachelor-thesis-slides/static/css/1.a8349495.chunk.css"
+  },
+  {
+    "revision": "43973a7fcdfbcfecc426",
+    "url": "/bachelor-thesis-slides/static/js/1.43973a7f.chunk.js"
+  },
+  {
+    "revision": "0e1e32df74087235425e",
+    "url": "/bachelor-thesis-slides/static/js/runtime~main.0e1e32df.js"
+  },
+  {
+    "revision": "9cd235c4d598e4dd252b0e0946c1b892",
+    "url": "/bachelor-thesis-slides/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/bachelor-thesis-slides/precache-manifest.7a404d1e40560f4db4f9f5c7d0d34021.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/bachelor-thesis-slides/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
